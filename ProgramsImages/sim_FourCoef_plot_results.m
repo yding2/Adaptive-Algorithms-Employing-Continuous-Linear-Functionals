85d5f903c209f373f2d021e4df615eb8e01a3fac
YuhanDing <jinliang98@hotmail.com>
2018-08-25 16:23:20 -0400

    Updates from ShareLaTeX

--- ProgramsImages/sim_FourCoef_plot_results.m
+++ ProgramsImages/sim_FourCoef_plot_results.m
@@ -8,30 +8,28 @@ load(['sim_FourCoef_results_basis_' basisName '.mat'], ...
 
 gail.InitializeDisplay %add some variables for nice plotting
 
-rng(47)
 figure
 log10epsVec = log10(eps_vec);
-scatter(rat_vec,n_vec,800,log10epsVec,'.'); %plot ratio of actual error to tolerance, with color corresonding to tolerance
-%h = scatter(rat_vec,n_vec,800,log10epsVec,'.'); %plot ratio of actual error to tolerance, with color corresonding to tolerance
+h = scatter(rat_vec,n_vec,800,log10epsVec,'.'); %plot ratio of actual error to tolerance, with color corresonding to tolerance
 hold on
 set(gca,'XScale','log', 'YScale','log')
 xlim([min([10.^floor(log10(rat_vec*0.8)); 0.1]) ...
  max([10.^(ceil(log10(rat_vec*1.2))); 1])])
 ylim(10.^[floor(log10(min(n_vec))) ceil(log10(max(n_vec)))])
-xlabel({'\(||f-\hat{f}||_{\infty}/\varepsilon\)'})
-ylabel({'Sample size, \(n_{j^{\dagger}}\)'})
+%xlabel({'\(||f-\hat{f}||_{\infty}/\varepsilon\)'})
+ylabel({'Sample size \(n\)'})
 hcb = colorbar; %showing tolerance values
 title(hcb,'\(\varepsilon\)','interpreter','latex')
 tickVals = floor(min_log10_eps):ceil(max_log10_eps);
 tickLabels = 10.^tickVals;
 set(hcb,'Ticks',tickVals,'TickLabels',tickLabels, ...
    'Limits',[tickVals(1) tickVals(end)])
-%[~,leg_icons] = legend(h, ...
-%    {'\(||\partial^{\{1\}}f-\partial^{\{1\}}f_{\mbox{app}}||_{2}/\varepsilon\) \quad', ...
-%    }, ...
-%    'box','off','location','north','orientation','horizontal');
+[~,leg_icons] = legend(h, ...
+   {'\(||\partial^{\{1\}}f-\partial^{\{1\}}f_{\mbox{app}}||_{2}/\varepsilon\) \quad', ...
+   }, ...
+   'box','off','location','north','orientation','horizontal');
 set(gcf,'Position',[200,200,1000,500]) %make figure big enough and the right aspect ratio
-%leg_icons(2).Children.MarkerSize = 30; %make legend icons large enough
+leg_icons(2).Children.MarkerSize = 30; %make legend icons large enough
 print -depsc SimFourCoefErr.eps
 
 %% Visualize (only a 2-d projection)
@@ -47,7 +45,7 @@ x_Vis = nom_Val*ones(n_Vis,d);
 x_Vis(:,xcoord) = xx(:);
 x_Vis(:,ycoord) = yy(:);
 
-%Input function
+%Input funcitons
 f_true_Vis = inp_basis_fun(wv_num_mtx,x_Vis) * (four_coef .* lambda(wv_num_mtx));
 m = length(eps_vec);
 range_k = wh_ordLambda(1:n_vec(m));
@@ -67,10 +65,8 @@ Sf_true_Vis = reshape(Sf_true_Vis,[n_grd_val n_grd_val]);
 surf(x_grd,x_grd,Sf_true_Vis); shading interp
 xlabel(['\(x_{' int2str(xcoord) '}\)'])
 ylabel(['\(x_{' int2str(ycoord) '}\)'])
-zlabel(['\(S(f)(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-   '}, ' num2str(nom_Val) ')\)'])
-% zlabel(['\(\partial^{\{1\}}f(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-%    '}, ' num2str(nom_Val) ', \ldots)\)'])
+zlabel(['\(\partial^{\{1\}}f(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
+   '}, ' num2str(nom_Val) ', \ldots)\)'])
 print -depsc SimFourCoefSol.eps
 
 
@@ -80,10 +76,8 @@ Sf_app_Vis = reshape(Sf_app_Vis,[n_grd_val n_grd_val]);
 surf(x_grd,x_grd,Sf_app_Vis); shading interp
 xlabel(['\(x_{' int2str(xcoord) '}\)'])
 ylabel(['\(x_{' int2str(ycoord) '}\)'])
-zlabel(['\(\tilde{A}(f,\varepsilon)(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-   '}, ' num2str(nom_Val) ')\)'])
-% zlabel(['\(\partial^{\{1\}}f_{\mbox{app}}(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-%    '}, ' num2str(nom_Val) ', \ldots)\)'])
+zlabel(['\(\partial^{\{1\}}f_{\mbox{app}}(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
+   '}, ' num2str(nom_Val) ', \ldots)\)'])
 print -depsc SimDirectSolAppx.eps
 
 figure
@@ -92,10 +86,8 @@ err_Vis = Sf_true_Vis - Sf_app_Vis;
 surf(x_grd,x_grd,err_Vis); shading interp
 xlabel(['\(x_{' int2str(xcoord) '}\)'])
 ylabel(['\(x_{' int2str(ycoord) '}\)'])
-zlabel(['\((S(f) - \tilde{A}(f))(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-   '}, ' num2str(nom_Val) ')\)'])
-% zlabel(['\(\partial^{\{1\}}f_{\mbox{err}}(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-%    '}, ' num2str(nom_Val) ', \ldots)\)'])
+zlabel(['\(\partial^{\{1\}}f_{\mbox{err}}(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
+   '}, ' num2str(nom_Val) ', \ldots)\)'])
 print -depsc SimDirectSolErr.eps
 
 
@@ -106,9 +98,7 @@ surf(x_grd,x_grd,f_true_Vis); shading interp
 xlabel(['\(x_{' int2str(xcoord) '}\)'])
 ylabel(['\(x_{' int2str(ycoord) '}\)'])
 zlabel(['\(f(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-   '}, ' num2str(nom_Val) ')\)'])
-% zlabel(['\(f(x_{' int2str(xcoord) '}, x_{' int2str(ycoord) ...
-%    '}, ' num2str(nom_Val) ', \ldots)\)'])
+   '}, ' num2str(nom_Val) ', \ldots)\)'])
 print -depsc SimFourCoefInpFun.eps
 
 
